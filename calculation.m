01535d3ab86fcdf6bf6cc2b8856678bfe36d7780
AayushJ25 <aayushj3@illinios.edu>
2026-02-15 00:12:26 -0600

    12/15/26 made 2 new functions. one to check how many cells are alive next to each cell and one to use that function and permutate the cells in the correct way. However I think the first function the value is backwards because 0 acutally means alive and 1 means dead.

--- calculation.m
+++ calculation.m
@@ -22,7 +22,34 @@ function corrected = RandomZO(mapSize)
 corrected = A;
 end 
 
-A = RandomZO(mapSize);
+function newNumber = numberAlive(i, j, matrix)
+    kernal = [1 1 1; 1 0 1; 1 1 1;];
+    neighbor = conv2(matrix, kernal, "same");
+    newNumber = neighbor(i,j);
+end
+
+
+
+function nextGen = process(matrix, mapSize)
+    C = matrix;
+    for i=1:mapSize
+        for j=1:mapSize
+            if matrix(i,j) == 1 & numberAlive(i,j,C) == 3
+                nextGen(i,j) = 0; % Cell becomes alive
+            end
+
+            if matrix(i,j) == 0 & (numberAlive(i,j,C) > 3 | numberAlive(i,j,C) <2)
+                nextGen(i,j) = 1; % Cell dies
+            end
+        end
+    end
+end
+
+
+
+A = ones(mapSize);
+
+
 % Display the generated map as an image
 
 figure;
@@ -33,10 +60,10 @@ title('Random Binary Map');
 
 
 while true
-    A = RandomZO(mapSize);
+    A = process(A, mapSize);
     set(h,'CData',A);
     drawnow;
-    pause(0.1);
+    pause(1);
 end
   
     
